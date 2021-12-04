55b0ddbc03d028f337a721161a83eff6ad1b9265
evo4np <anilpdlx@gmail.com>
2021-12-04 17:43:34 +0100

    solutions fot day one

---
+++ day1.py
@@ -0,0 +1,27 @@
+
+
+def problem_one(values):
+    increased = 0
+
+    for i in range(1, len(values)):
+        if values[i] > values[i - 1]:
+            increased += 1
+    print(increased)
+
+
+def problem_two(values):
+    increased = 0
+
+    for i in range(2, len(values) - 1):
+        if (values[i] + values[i - 1] + values[i - 2]) < (values[i + 1] + values[i] + values[i - 1]):
+            increased += 1
+    print(increased)
+
+
+if __name__ == '__main__':
+
+    with open('day_one.txt') as f:
+        data = [int(line.strip()) for line in f]
+
+    # problem_one(data)
+    problem_two(data)
