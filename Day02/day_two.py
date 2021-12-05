23fd908eed28f42a35cb165bbcb32bf42aecb570
evo4np <anilpdlx@gmail.com>
2021-12-05 14:37:04 +0100

    Day2 problem1

---
+++ Day02/day_two.py
@@ -0,0 +1,24 @@
+def problem_one(datas):
+    horizontal = 0
+    depth = 0
+    for data in datas:
+        option = data.split()[0]
+        value = int(data.split()[1])
+
+        if option == 'forward':
+            horizontal += value
+        elif option == 'down':
+            depth += value
+        elif option == 'up':
+            depth -= value
+    result = horizontal * depth
+    print(result)
+
+
+if __name__ == '__main__':
+
+    with open('day_two_puzzle.txt') as f:
+        puzzle_data = [str(line.strip()) for line in f]
+
+    problem_one(puzzle_data)
+    # problem_two(data)
